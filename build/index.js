bf7fe6625765120d114a145b441d5a025f83173a
Nilo Dornellas <iannilo01@gmail.com>
2025-02-15 20:43:08 -0300

    First commit

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+_reactDom["default"].render(/*#__PURE__*/_react["default"].createElement("h1", {
+  id: "title"
+}, "Ol\xE1 mundo!"), document.getElementById('root'));
